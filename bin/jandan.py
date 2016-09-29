ee9e96ba1b64a3d27281c1e6a5bc32c361140524
chengongliang <chengongliang7@gmail.com>
2016-09-29 13:49:55 +0800

    fix

--- bin/jandan.py
+++ bin/jandan.py
@@ -5,15 +5,15 @@
 import re
 import os
 import sys
-#import random
 import requests
 from sql import mySQL
 from conf import setting
 
 s = mySQL()
+
 def getHtml(url):
+    #获取html
     n = 0
-    #agent = setting.configs['agents']
     agent = s.get_agent()
     while n < len(agent):
         headers = {'User-Agent':agent[n]}
@@ -29,6 +29,7 @@ def getHtml(url):
 
 
 def getUrl_list(html,p):
+    #获取图片链接
     re_img = re.compile(r'<div class="text">.*?<p>.*?<a href="(http://ww.*?)".*?<div class="vote".*?<span id="cos_support.*?">(\d+)</span>.*?<span id="cos_unsupport.*?">(\d+)</span>', re.S)
     url_list = []
     items = re_img.findall(html)
@@ -39,29 +40,22 @@ def getUrl_list(html,p):
     return url_list
 
 def getCurrent():
+    #获取最新页码
     start_page = getHtml("http://jandan.net/ooxx")
     current = re.compile(r'<div class="comments">.*?<span class="current-comment-page">\[(\d+)\]</span>',re.S)
     page = current.findall(start_page)[0]
-    #s = mySQL()
     s.put_num('page_num',page)
-    #with open('page_num','w') as fd:
-    #    fd.write(str(page))
     return page
 
-#def getLast():
-#    if os.path.exists('conf/page_num'):
-#        with open('conf/page_num','r') as fd:
-#            return fd.read()
-#    else:
-#        return getCurrent()
-
 def save_img(url,name):
+    #保存图片
     if not os.path.exists('src/%s' %name):
         with open('src/%s' %name, 'wb') as fd:
             img = getHtml(url)
             fd.write(img)
 
 def download(url_list):
+    #切分链接，获取图片名
     for url in url_list:
         name = url.split('/')[-1]
         save_img(url,name)
--- bin/sql.py
+++ bin/sql.py
@@ -15,36 +15,28 @@ class mySQL():
             sys.exit(1)
 
     def get_agent(self):
+        #获取agent列表
         self.cur.execute("select info from configs where config='agent'")
         sql_data = self.cur.fetchall()
         List = sql_data[0][0].split("','")
         return List
 
     def get_num(self):
+        #获取上次爬取的页码
         self.cur.execute("select info from configs where config='page_num'")
         num = self.cur.fetchall()[0][0]
         return num
 
     def put_num(self,conf,num):
+        #写入新页码
         self.cur.execute("UPDATE `jandan`.`configs` SET `info`=%s WHERE (`config`=%s)",[num, conf])
         self.conn.commit()
 
     def put_ooxx(self,num,url,oo,xx):
+        #写入图片相关信息
         self.cur.execute("INSERT INTO `jandan`.`ooxx` (`page_num`, `url`, `oo`, `xx`) SELECT %s, %s, %s, %s FROM dual WHERE NOT EXISTS (SELECT url FROM ooxx WHERE url = %s)",[num,url,oo,xx,url])
         self.conn.commit()
 
     def close(self):
         self.cur.close()
         self.conn.close()
-
-#if __name__ == '__main__':
-#    s = mySQL()
-#    l = s.get_agent()
-#    num = s.get_num()
-#    print num
-#    print l[0]
-#    print "_*" * 20
-#    print l[1]
-#    print "_*" * 20
-#    s.put_num('page_num','2017')
-#    s.close()
