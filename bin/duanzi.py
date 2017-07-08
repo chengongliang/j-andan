7e25177a4e1018ca51f6e38aeefd7681938a8b5c
chengongliang <chengongliang@01zhuanche.com>
2017-07-08 14:58:55 +0800

    修复段子正则

--- bin/duanzi.py
+++ bin/duanzi.py
@@ -10,7 +10,7 @@ from ooxx import s, getHtml, getCurrent
 def parse(url):
     data = getHtml(url)
     re_content = re.compile(
-        r'<div class="text">.*?<p>(.*?)</p>.*?<div class="vote".*?<span id="cos_support.*?">(\d+)</span>.*?<span id="cos_unsupport.*?">(\d+)</span>', re.S)
+        r'<div class="text">.*?<p>(.*?)</p>.*?<div class="jandan-vote".*?<span class="tucao-like-container">.*?\[<span>(\d+)</span>].*?<span class="tucao-unlike-container">.*?\[<span>(\d+)</span>\]', re.S)
     items = re_content.findall(data)
     replaceBR = re.compile(r'<br />')
     # new_content = []
@@ -21,7 +21,7 @@ def parse(url):
                 md5 = hashlib.md5(c).hexdigest()[:15]
                 print c
                 print md5 + "\n"
-                # s.put_duan(page_num,c,oo,xx,md5)
+                s.put_duan(page_num, c, oo, xx, md5)
         except ZeroDivisionError:
             c = replaceBR.sub('\n', c)
 
@@ -30,8 +30,8 @@ if __name__ == '__main__':
     num_info = 'duan_page_num'
     start_page = 'http://jandan.net/duan'
     last_num = s.get_num(num_info)
-    # current_num = getCurrent(start_page,num_info)
-    current_num = 1557
+    current_num = getCurrent(start_page, num_info)
+    # current_num = 1557
     list_pages = range(int(last_num), int(current_num) + 1)
     print list_pages
     for page_num in list_pages:
