69ab070bf693ab67c6519a97c0120d1288846616
chengongliang <chengongliang@01zhuanche.com>
2017-08-11 16:05:41 +0800

    update

--- bin/duanzi.py
+++ bin/duanzi.py
@@ -4,7 +4,10 @@
 
 import re
 import hashlib
+from wechat import WE
 from ooxx import s, getHtml, getCurrent
+
+w = WE()
 
 
 def parse(url):
@@ -14,25 +17,34 @@ def parse(url):
     items = re_content.findall(data)
     replaceBR = re.compile(r'<br />')
     # new_content = []
+    dz_list = []
     for c, oo, xx in items:
         try:
             if int(oo) / int(xx) >= 10:
-                c = replaceBR.sub('\n', c).strip()
+                # c = replaceBR.sub('\n', c).strip()
+                c = replaceBR.sub('', c).strip()
+                dz_list.append(c)
                 md5 = hashlib.md5(c).hexdigest()[:15]
-                print c
-                print md5 + "\n"
+                # print c
+                # print md5 + "\n"
                 s.put_duan(page_num, c, oo, xx, md5)
         except ZeroDivisionError:
             c = replaceBR.sub('\n', c)
+    content = '\n--------- --------- ---------\n'.join(dz_list[1:-1])
+    # print content
+    w.senddata(content)
 
 
 if __name__ == '__main__':
     num_info = 'duan_page_num'
     start_page = 'http://jandan.net/duan'
-    last_num = s.get_num(num_info)
-    current_num = getCurrent(start_page, num_info)
+    last_num = int(s.get_num(num_info))
+    new_num = last_num + 1
+    # current_num = (getCurrent(start_page, num_info))
     # current_num = 1557
-    list_pages = range(int(last_num), int(current_num) + 1)
+    # list_pages = range(int(last_num), int(current_num) + 1)
+    list_pages = range(last_num, new_num)
+    s.put_num(num_info, new_num)
     print list_pages
     for page_num in list_pages:
         url = 'http://jandan.net/duan/page-%s#comments' % page_num
